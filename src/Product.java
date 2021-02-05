01c6a497a8e2f37dc212ae3c8f69c0abb17430d9
Nathan <nathan@dhcp-67-168.cu-internet.carleton.ca>
2021-02-05 15:10:07 -0500

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,33 @@
+public class Product {
+
+    private String name;
+    private int id;
+    private float price;
+
+    public Product(String newName, int newId, float newPrice) {
+
+        this.name = newName;
+        this.id = newId;
+        this.price = newPrice;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public float getPrice() {
+
+        return price;
+
+    }
+    
+}
